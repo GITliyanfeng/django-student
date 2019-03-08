140072a35fe50c98121305a312cd73d59ba7440f
liyanfeng <957012567@qq.com>
2019-03-09 02:00:32 +0800

    add middleware to count time for a request => response
    rm ./idea/*

--- StudentManager/settings.py
+++ StudentManager/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'student.middlewares.counttime.TimeItMiddleware',
 ]
 
 ROOT_URLCONF = 'StudentManager.urls'
